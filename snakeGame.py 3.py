cc1dff4b8bb13aabc91320d2d3d00f59c432b8ab
com3226 <jyh051026@gmail.com>
2024-11-04 14:16:43 +0900

    뱀 그리기

---
+++ snakeGame.py 3.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+# colors
+BLUE = (0,0,255); RED = (255,0,0)
+
+#screen
+SCR_WIDTH, SCR_HEIGHT = 400, 200
+screen = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+pygame.display.set_caption('Snake Game')
+
+#snake
+snake_size = 10
+
+running = True
+while running:
+    for event in pygame.event.get():
+        #print(event)
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.draw.rect(screen, BLUE, [int(SCR_WIDTH/2 - snake_size/2),\
+                                    int(SCR_HEIGHT/2 - snake_size/2),\
+                                    snake_size, snake_size])
+    pygame.display.update()
+
+pygame.quit()
